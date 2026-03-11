f5e8785915b2a6a35c9990b33631e64d2f74815c
atfahida <atfahida@gmail.com>
2026-03-11 20:20:46 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,33 @@
+print("welcome")
+x=10
+print(x)
+
+a,b,c=10,2,23
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+
+z=x=29
+print(z)
+print(x)
+
+A=2.3
+print(int(A))
+
+
+print(bool(1))
+print(bool(0))
+print(bool(" "))
+print(bool("1"))
+print(bool(None))
+
+year=int(input("enter your birth year"))
+age=2026-year
+print(age)
+
+birth_year = input("Enter your birth year: ")
+
+age = 2026 - int(birth_year)
+
+print("Your Age is:", age)
